9d8ea770bd15f90f56a13da1a66d02e0f218171b
Aaron Sujar <aaronsujar@gmail.com>
2017-06-07 20:15:48 +0100

    feat: Video in the intro donfeat: Video in the intro donee

---
+++ Assets/Scripts/VR/VideoCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCollider : MonoBehaviour
+{
+
+
+    public Playback m_videoplayer;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hola");
+        if (other.CompareTag("MainCamera"))
+            m_videoplayer.Play();
+    }
+}
